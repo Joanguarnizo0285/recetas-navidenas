4f83edb3d44ca328401226a13f3e5acf77ab6a13
Joanguarnizo0285 <jd.guarnizo285@gmail.com>
2024-12-12 16:42:24 -0500

    first commit

---
+++ backend/src/routes/recipeRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+    getAllRecipes,
+    addRecipe,
+    getRecipeById,
+} from '../controllers/recipeController';
+
+
+const router = Router();
+
+router.get('/', getAllRecipes);
+router.post('/', addRecipe);
+router.get('/:id', getRecipeById);
+
+export default router;
+
